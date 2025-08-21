6c4fd41b9bb340fe6029e7d40a996500a56d0058
Surya-V-28 <Surya-V-28@users.noreply.github.com>
2025-08-21 09:20:41 +0900

    Implement Oracle integration

---
+++ src/hooks/Oracle/index.js
@@ -0,0 +1,4 @@
+export { default as useRequestValuationUpdate } from "../Properties/useRequestValuationUpdate";
+export { default as useOracleStatus } from "../Properties/useOracleStatus";
+export { default as useOracleEvents } from "../Properties/useOracleEvents";
+export { default as useOracleAdmin } from "../Properties/useOracleAdmin"; 
